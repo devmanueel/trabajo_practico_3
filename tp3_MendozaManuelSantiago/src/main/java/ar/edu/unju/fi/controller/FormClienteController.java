60bc5def34c357bafbbc0eb2d1de85ff01539121
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-05-16 19:32:23 -0300

    modificaciones en index.hmtl y agregacion de form-cliente.hmtl con el archivo styles.css

---
+++ tp3_MendozaManuelSantiago/src/main/java/ar/edu/unju/fi/controller/FormClienteController.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FormClienteController {
+	@GetMapping({"/formulario"}) //indica en la raiz del servidor
+		
+		public String getFormulario() {
+			return "form-cliente.html";
+		}
+}
